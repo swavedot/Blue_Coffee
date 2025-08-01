c4e11116272511a2b6f913e31805e45cc3843202
swavedot <sanghun131@gmail.com>
2025-08-01 17:52:59 +0900

    주석

--- js/member.js
+++ js/member.js
@@ -11,20 +11,11 @@ document.addEventListener('DOMContentLoaded', () => {
       // 모든 항목 닫기
       faqItems.forEach(i => {
         i.classList.remove('active');
-        const a = i.querySelector('.faq-answer');
-        a.style.maxHeight = '0';
-        a.style.paddingTop = '0';
-        a.style.paddingBottom = '0';
-        a.style.paddingLeft = '20px';
       });
 
       // 클릭한 항목만 열기
       if (!isActive) {
         item.classList.add('active');
-        answer.style.maxHeight = '300px';
-        answer.style.paddingTop = '20px';
-        answer.style.paddingBottom = '20px';
-        answer.style.paddingLeft = '20px';
       }
     });
   });
